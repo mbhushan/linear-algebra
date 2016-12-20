709f20ea8b1c7557abd01634f2a83fcd36bc3471
manibhushan <manibhushan.cs@gmail.com>
2016-12-19 21:16:27 -0800

    testing line class init

--- ud953/vectors_ops/LineClient.py
+++ ud953/vectors_ops/LineClient.py
@@ -1,9 +1,14 @@
+from ud953.vectors_ops.Vector import Vector
+from ud953.vectors_ops.Line import Line
 
-
+def line_test():
+    v = Vector(['3', '2'])
+    L = Line(normal_vector=Vector(['3', '2']), constant_term='5')
+    print(L)
 
 
 def main():
-    None
+    line_test()
 
 
 if __name__ == '__main__':
