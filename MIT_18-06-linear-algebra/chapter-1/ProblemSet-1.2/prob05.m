cf63fc08a25222dadf267d148782ca0324db7cb5
manibhushan <manibhushan.cs@gmail.com>
2017-01-30 06:46:24 -0800

    prob 5 of problem set 1.2

---
+++ MIT_18-06-linear-algebra/chapter-1/ProblemSet-1.2/prob05.m
@@ -0,0 +1,6 @@
+%{
+5. Find two points on the line of intersection of the three planes t = 0 and z = 0 and
+x+y+z+t = 1 in four-dimensional space.	
+%}
+
+% Ans: the two points are (x=1, y=0, z=0, t=0) and (x=0, y=1, z=0, t=0)
