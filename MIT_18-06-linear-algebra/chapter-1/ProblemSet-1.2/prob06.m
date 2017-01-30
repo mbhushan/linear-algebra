a528958cbf8f93dc41e6a6c94459d06bc6b7befa
manibhushan <manibhushan.cs@gmail.com>
2017-01-30 06:46:52 -0800

    prob 6 of problem set 1.2

---
+++ MIT_18-06-linear-algebra/chapter-1/ProblemSet-1.2/prob06.m
@@ -0,0 +1,14 @@
+%{
+6. When b = (2;5;7), find a solution (u;v;w) to equation (4) different from the solution
+(1;0;1) mentioned in the text.
+%}
+
+% ======== ANS ==========
+#{
+{v == 1/3 - u/3, w == 5/3 - (2 u)/3}
+
+u = 7,
+v = -2,
+w = -3
+
+#}
